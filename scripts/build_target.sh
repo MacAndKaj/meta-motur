be20f4acdfad91c02e5ccfc5ecd3d16be4d62e91
MacAndKaj <mkajdak@gmail.com>
2024-07-15 00:56:58 +0200

    Build structure change

--- scripts/build_target.sh
+++ scripts/build_target.sh
@@ -2,9 +2,34 @@
 # <path>/build_target.sh <path_to_root_dir> <machine> <target>
 
 # machine - setup from yocto (qemuarm64/qemux86-64/raspberrypi3-64)
-MACHINE=$2
+ROOTDIR_PATH=.
+MACHINE=qemux86-64
 DISTRO=poky
+TARGET=core-image-minimal
 
-source "$1"/poky/oe-init-build-env
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -m|--machine)
+      MACHINE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--path)
+      ROOTDIR_PATH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -t|--target)
+      TARGET="$2"
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
 
-bitbake "$3"
+source "$ROOTDIR_PATH"/poky/oe-init-build-env
+
+bitbake "$TARGET"
