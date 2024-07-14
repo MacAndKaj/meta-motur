77fd4dc0e5823b6bdee73175fffd2606cd10b17a
MacAndKaj <mkajdak@gmail.com>
2024-07-15 00:50:17 +0200

    Build structure change

--- scripts/build_target.sh
+++ scripts/build_target.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # <path>/build_target.sh <path_to_root_dir> <machine> <target>
 
-source "$1"/poky/oe-init-build-env
-
 # machine - setup from yocto (qemuarm64/qemux86-64/raspberrypi3-64)
-export MACHINE=$2
-export DISTRO=poky
+MACHINE=$2
+DISTRO=poky
+
+source "$1"/poky/oe-init-build-env
 
 bitbake "$3"
