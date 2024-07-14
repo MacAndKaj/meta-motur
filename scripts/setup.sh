00855324b0aee0ff7dcf6114db0302eee0374425
MacAndKaj <mkajdak@gmail.com>
2024-07-14 23:27:04 +0200

    Build structure change

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,13 +9,13 @@ git clone https://git.yoctoproject.org/poky -b $META_BRANCH
 
 source poky/oe-init-build-env
 
-bitbake-layers add-layer ../meta-openembedded/meta-oe
-bitbake-layers add-layer ../meta-openembedded/meta-python
-bitbake-layers add-layer ../meta-openembedded/meta-networking
-bitbake-layers add-layer ../meta-ros/meta-ros-common
-bitbake-layers add-layer ../meta-ros/meta-ros2
-bitbake-layers add-layer ../meta-ros/meta-ros2-humble
-bitbake-layers add-layer ../meta-motur
+bitbake-layers add-layer "$1"/meta-openembedded/meta-oe
+bitbake-layers add-layer "$1"/meta-openembedded/meta-python
+bitbake-layers add-layer "$1"/meta-openembedded/meta-networking
+bitbake-layers add-layer "$1"/meta-ros/meta-ros-common
+bitbake-layers add-layer "$1"/meta-ros/meta-ros2
+bitbake-layers add-layer "$1"/meta-ros/meta-ros2-humble
+bitbake-layers add-layer "$1"/meta-motur
 
 # machine - setup from yocto (qemuarm64/qemux86-64/raspberrypi3-64)
 export MACHINE=$1
